05f223ea291b7a54d6155d9138458325616724b1
nuxsh <nuxshdev@outlook.com>
2021-07-03 10:47:25 +0530

    bootstrap: initial groundwork

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,16 @@
 # this script installs all the stuff required for my configs
 # once i finish writing it, that is.
 # alas, for now, it is left unfinished
+
+if [[ $(uname) == "Linux" ]]; then
+  echo "you are using linux"
+  if [[ $(cat /etc/issue | cut -d' ' -f1-2) == "Arch Linux" ]]; then
+    echo "you are using arch linux"
+  else
+    echo "there was an error determining which linux distro you use."
+  fi
+elif [[ $(uname) == "Darwin" ]]; then
+  echo "you are using macos"
+else
+  echo "There was an issue while trying to find out which OS you use."
+fi
