124be68151c9cd6fe503d265667689c5c64368c7
nuxsh <nuxshdev@outlook.com>
2021-07-06 21:57:50 +0530

    feat: add mapping to open lazygit in nvim

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -18,3 +18,25 @@ end
 map("n", "<leader>pu", [[ <Cmd> PackerUpdate<CR>]])
 map("n", "<leader>pc", [[ <Cmd> PackerCompile<CR>]])
 map("n", "<leader>ps", [[ <Cmd> PackerSync<CR>]])
+
+-- lazygit
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "single",
+  },
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+map("n", "<leader>g", [[ <Cmd> lua _lazygit_toggle()<CR>]])
