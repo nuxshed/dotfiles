a975936f526aff82683fcbf74dfbd8bf50352ae2
nuxsh <nuxshdev@outlook.com>
2021-07-15 16:07:52 +0530

    fix: gitsigns config idk what caused it exactly, but it was solved by pasting the default config and editing the required parts

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,80 +1,70 @@
 require("gitsigns").setup({
-  signs = {
-    add = {
-      hl = "GitSignsAdd",
-      text = "+",
-      numhl = "GitSignsAddNr",
-      linehl = "GitSignsAddLn",
-    },
-    change = {
-      hl = "GitSignsChange",
-      text = "~",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
-    },
-    delete = {
-      hl = "GitSignsDelete",
-      text = "_",
-      show_count = true,
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
-    },
-    topdelete = {
-      hl = "GitSignsDelete",
-      text = "‾",
-      show_count = true,
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
-    },
-    changedelete = {
-      hl = "GitSignsChange",
-      text = "~",
-      show_count = true,
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
-    },
-  },
-  count_chars = {
-    [1] = "",
-    [2] = "₂",
-    [3] = "₃",
-    [4] = "₄",
-    [5] = "₅",
-    [6] = "₆",
-    [7] = "₇",
-    [8] = "₈",
-    [9] = "₉",
-    ["+"] = "₊",
-  },
-  numhl = false,
-  linehl = false,
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
+	signs = {
+		add = {
+			hl = "GitSignsAdd",
+			text = "+",
+			numhl = "GitSignsAddNr",
+			linehl = "GitSignsAddLn",
+		},
+		change = {
+			hl = "GitSignsChange",
+			text = "~",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
+		},
+		delete = {
+			hl = "GitSignsDelete",
+			text = "-",
+			numhl = "GitSignsDeleteNr",
+			linehl = "GitSignsDeleteLn",
+		},
+		topdelete = {
+			hl = "GitSignsDelete",
+			text = "‾",
+			numhl = "GitSignsDeleteNr",
+			linehl = "GitSignsDeleteLn",
+		},
+		changedelete = {
+			hl = "GitSignsChange",
+			text = "~",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
+		},
+	},
+	numhl = false,
+	linehl = false,
+	keymaps = {
+		-- Default keymap options
+		noremap = true,
+		buffer = true,
 
-    ["n ]c"] = {
-      expr = true,
-      "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
-    },
-    ["n [c"] = {
-      expr = true,
-      "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
-    },
+		["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
+		["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
 
-    ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ["n <leader>hR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-  },
-  watch_index = { interval = 1000 },
-  current_line_blame = false,
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  use_decoration_api = true,
-  use_internal_diff = true,
-  yadm = { enable = true },
+		["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+		["v <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+		["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+		["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+		["v <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+		["n <leader>hR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+		["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+		["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+
+		-- Text objects
+		["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+		["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+	},
+	watch_index = {
+		interval = 1000,
+		follow_files = true,
+	},
+	current_line_blame = false,
+	current_line_blame_delay = 1000,
+	current_line_blame_position = "eol",
+	sign_priority = 6,
+	update_debounce = 100,
+	status_formatter = nil, -- Use default
+	word_diff = false,
+	use_decoration_api = true,
+	use_internal_diff = true, -- If luajit is present
 })
