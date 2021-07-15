a504112730c6d5895f29a3f4da8315ecd973d81a
nuxsh <nuxshdev@outlook.com>
2021-07-15 18:21:06 +0530

    nvim: remove <C-a> mapping to yank all

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -4,14 +4,13 @@ local map = utils.map
 map("n", "<leader>n", [[ <Cmd> set nu!<CR> ]])
 map("n", "<leader>nr", [[ <Cmd> set relativenumber!<CR> ]])
 map("n", "<leader>s", [[ <Cmd> set spell!<CR> ]])
-map("n", "<C-a>", [[ <Cmd> %y+<CR> ]])
 
 -- clear search highlight on enter
 map("n", "<CR>", [[ <Cmd> nohl<CR>]])
 
 -- easily switch tabs
 for i = 1, 9 do
-  map("n", "<A-" .. i .. ">", ':lua require"bufferline".go_to_buffer(' .. i .. ")<CR>")
+	map("n", "<A-" .. i .. ">", ':lua require"bufferline".go_to_buffer(' .. i .. ")<CR>")
 end
 
 -- Packer Commands
@@ -22,20 +21,20 @@ map("n", "<leader>ps", [[ <Cmd> PackerSync<CR>]])
 -- lazygit
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
-  cmd = "lazygit",
-  dir = "git_dir",
-  direction = "float",
-  float_opts = {
-    border = "single",
-  },
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-  end,
+	cmd = "lazygit",
+	dir = "git_dir",
+	direction = "float",
+	float_opts = {
+		border = "single",
+	},
+	on_open = function(term)
+		vim.cmd("startinsert!")
+		vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+	end,
 })
 
 function _lazygit_toggle()
-  lazygit:toggle()
+	lazygit:toggle()
 end
 
 -- vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
