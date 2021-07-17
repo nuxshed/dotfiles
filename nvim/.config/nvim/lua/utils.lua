11ab0ec3f4edf9fc86bdc04d08eb2508c6a9beed
nuxsh <nuxshdev@outlook.com>
2021-07-17 12:52:03 +0530

    nvim: add function to check for git workspace

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -76,4 +76,25 @@ function M.has_width_gt(cols)
   return vim.fn.winwidth(0) / 2 > cols
 end
 
+function M.check_git_workspace()
+  local get_git_dir = require("galaxyline.provider_vcs").get_git_dir
+  if vim.bo.buftype == "terminal" then
+    return false
+  end
+  local current_file = vim.fn.expand("%:p")
+  local current_dir
+  -- if file is a symlinks
+  if vim.fn.getftype(current_file) == "link" then
+    local real_file = vim.fn.resolve(current_file)
+    current_dir = vim.fn.fnamemodify(real_file, ":h")
+  else
+    current_dir = vim.fn.expand("%:p:h")
+  end
+  local result = get_git_dir(current_dir)
+  if not result then
+    return false
+  end
+  return true
+end
+
 return M
