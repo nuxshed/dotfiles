444a81943b1d250eebdd220d8b432d990168b89e
nuxsh <nuxshdev@outlook.com>
2021-07-19 21:28:03 +0530

    fix mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,26 +8,19 @@ map("n", "<leader>s", [[ <Cmd> set spell!<CR> ]])
 
 map("n", "<leader>,", [[ <Cmd>edit ~/.config/nvim/lua/config.lua<CR>]])
 
+-- packer commands
+map("n", "<leader>pu", [[ <Cmd> PackerUpdate<CR>]])
+map("n", "<leader>ps", [[ <Cmd> PackerSync<CR>]])
+map("n", "<leader>pc", [[ <Cmd> PackerCompile<CR>]])
+map("n", "<leader>pi", [[ <Cmd> PackerInstall<CR>]])
+
 -- clear search highlight on enter
 map("n", "<CR>", [[ <Cmd> nohl<CR>]])
-
--- whichkey
-wk.register({
-  ["<leader>"] = {
-    p = {
-      name = "+packer",
-      u = { [[<Cmd>PackerUpdate<CR>]] },
-      s = { [[<Cmd>PackerSync<CR>]] },
-      c = { [[<Cmd>PackerCompile<CR>]] },
-      i = { [[<Cmd>PackerInstall<CR>]] },
-    },
-  },
-})
 
 -- easily switch tabs
 for i = 1, 9 do
   map("n", "<A-" .. i .. ">", ':lua require"bufferline".go_to_buffer(' .. i .. ")<CR>")
   wk.register({
-    ["<leader>" .. i] = "switch to buffer " .. i,
+    ["<A-" .. i .. ">"] = "switch to buffer " .. i,
   })
 end
