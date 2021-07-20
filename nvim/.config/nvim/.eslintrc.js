fad5a0f3e3471c2575a8b3a3d80f9fa26f0def0b
nuxsh <nuxshdev@outlook.com>
2021-07-20 11:45:15 +0530

    configure ts-utils & eslint_d

---
+++ nvim/.config/nvim/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    ecmaFeatures: {
+      blockBindings: true,
+      jsx: true,
+    },
+  },
+};
--- nvim/.config/nvim/lua/lsp/typescript.lua
+++ nvim/.config/nvim/lua/lsp/typescript.lua
@@ -1,6 +1,5 @@
 local lspconfig = require("lspconfig")
 local ts_utils = require("nvim-lsp-ts-utils")
-local utils = require("utils")
 
 local M = {}
 
@@ -10,10 +9,18 @@ local ts_utils_settings = {
   import_all_scan_buffers = 100,
   complete_parens = true,
   signature_help_in_parens = true,
+
+  -- eslint
   eslint_bin = "eslint_d",
   eslint_enable_diagnostics = true,
+  eslint_enable_code_actions = true,
+
+  -- formatting
   enable_formatting = true,
   formatter = "eslint_d",
+  eslint_config_fallback = vim.fn.stdpath("config") .. "/.eslintrc.js",
+
+  -- update imports on file move
   update_imports_on_move = true,
 }
 
