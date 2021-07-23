437cd9f5ea4f2835cdd7b11c095bbc4944aaab99
nuxsh <nuxshdev@outlook.com>
2021-07-23 13:17:43 +0530

    configure mardown and fences

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -49,6 +49,31 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 
+-- markdown
+-- Use proper syntax highlighting in code blocks
+local fences = {
+  "lua",
+  "vim",
+  "json",
+  "typescript",
+  "javascript",
+  "js=javascript",
+  "ts=typescript",
+  "shell=sh",
+  "python",
+  "sh",
+  "console=sh",
+}
+vim.g.markdown_fenced_languages = fences
+
+-- plasticboy/vim-markdown
+vim.g.vim_markdown_folding_level = 10
+vim.g.vim_markdown_fenced_languages = fences
+vim.g.vim_markdown_folding_style_pythonic = 1
+vim.g.vim_markdown_conceal_code_blocks = 0
+vim.g.vim_markdown_frontmatter = 1
+vim.g.vim_markdown_strikethrough = 1
+
 vim.o.grepprg =
   [[rg --hidden --glob "!.git" --no-heading --smart-case --vimgrep --follow $*]]
 vim.opt.grepformat = vim.opt.grepformat ^ { "%f:%l:%c:%m" }
