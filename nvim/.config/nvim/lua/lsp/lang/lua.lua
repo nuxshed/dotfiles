e3cfb28dd5593356b5ebee7317efab022f5b6844
nuxsh <nuxshdev@outlook.com>
2021-07-23 09:10:09 +0530

    fix lua-dev removes ability to toggle it via config.lua tho that seems to have been the problem

--- nvim/.config/nvim/lua/lsp/lang/lua.lua
+++ nvim/.config/nvim/lua/lsp/lang/lua.lua
@@ -1,29 +1,31 @@
 local M = {}
 
--- FIX: lua dev isn't working
 M.setup = function(on_attach)
-  require("lspconfig")["lua"].setup({
-    root_dir = vim.loop.cwd,
-    on_attach = on_attach,
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
+  require("lspconfig").lua.setup({
+    require("lua-dev").setup({}),
   })
+  -- require("lspconfig")["lua"].setup({
+  --   root_dir = vim.loop.cwd,
+  --   on_attach = on_attach,
+  --   settings = {
+  --     Lua = {
+  --       diagnostics = {
+  --         globals = { "vim" },
+  --       },
+  --       workspace = {
+  --         library = {
+  --           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+  --           [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+  --         },
+  --         maxPreload = 100000,
+  --         preloadFileSize = 10000,
+  --       },
+  --       telemetry = {
+  --         enable = false,
+  --       },
+  --     },
+  --   },
+  -- })
 end
 
 return M
