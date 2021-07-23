e440b8ed013f4c89388ca53389db16fd3d97cbc6
nuxsh <nuxshdev@outlook.com>
2021-07-23 12:00:58 +0530

    fix luadev this is the last time, i promise

--- nvim/.config/nvim/lua/lsp/lang/lua.lua
+++ nvim/.config/nvim/lua/lsp/lang/lua.lua
@@ -1,31 +1,59 @@
 local M = {}
 
 M.setup = function(on_attach)
-  -- require("lspconfig").lua.setup({
-  --   require("lua-dev").setup({}),
-  -- })
-  require("lspconfig")["lua"].setup {
-    root_dir = vim.loop.cwd,
-    on_attach = on_attach,
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+  local luadev = require("lua-dev").setup {
+    library = {
+      vimruntime = true,
+      types = true,
+      plugins = true,
+      -- plugins = { "nvim-treesitter", "plenary.nvim"},
+    },
+    lspconfig = {
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
           },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-        telemetry = {
-          enable = false,
+          workspace = {
+            library = {
+              [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+              [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+            },
+            maxPreload = 100000,
+            preloadFileSize = 10000,
+          },
+          telemetry = {
+            enable = false,
+          },
         },
       },
     },
   }
+
+  require("lspconfig").lua.setup(luadev)
+  -- require("lspconfig")["lua"].setup {
+  --   root_dir = vim.loop.cwd,
+  --   on_attach = on_attach,
+  --   settings = {
+  --     Lua = {
+  --       diagnostics = {
+  --         globals = { "vim" },
+  --       },
+  --       workspace = {
+  --         library = {
+  --           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+  --           [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+  --         },
+  --         maxPreload = 100000,
+  --         preloadFileSize = 10000,
+  --       },
+  --       telemetry = {
+  --         enable = false,
+  --       },
+  --     },
+  --   },
+  -- }
 end
 
 return M
