65117cf0ca78ecad3ceed583ad9141cf9f5cdf06
nuxsh <nuxshdev@outlook.com>
2021-07-26 17:47:13 +0530

    nvim: use Lspsaga for hover

--- nvim/.config/nvim/lua/lsp/commands.lua
+++ nvim/.config/nvim/lua/lsp/commands.lua
@@ -8,7 +8,6 @@ function M.setup()
   utils.lua_command("LspImplementation", "vim.lsp.buf.implementation()")
   utils.lua_command("LspReferences", "vim.lsp.buf.references()")
   utils.lua_command("LspRename", "vim.lsp.buf.rename()")
-  utils.lua_command("LspHover", "vim.lsp.buf.hover()")
   utils.lua_command("LspFormat", "vim.lsp.buf.formatting()")
   utils.command("LspLog", "edit " .. vim.lsp.get_log_path())
 end
