f0b5f216572f0a01ebeb52b40d39d91669828798
nuxsh <nuxshed@gmail.com>
2021-08-19 20:25:23 +0530

    nvim: better terminal colors

--- nvim/.config/nvim/lua/highlights.lua
+++ nvim/.config/nvim/lua/highlights.lua
@@ -92,6 +92,24 @@ cmd("hi whitespace guifg=" .. colors.grey)
 -- the background color for neovim
 cmd("hi Normal guibg=" .. colors.bg)
 
+-- terminal
+vim.g.terminal_color_0 = colors.bg
+vim.g.terminal_color_8 = colors.grey
+vim.g.terminal_color_7 = colors.fg
+vim.g.terminal_color_15 = colors.fg
+vim.g.terminal_color_1 = colors.red
+vim.g.terminal_color_9 = colors.red
+vim.g.terminal_color_2 = colors.green
+vim.g.terminal_color_10 = colors.green
+vim.g.terminal_color_3 = colors.yellow
+vim.g.terminal_color_11 = colors.yellow
+vim.g.terminal_color_4 = colors.blue
+vim.g.terminal_color_12 = colors.blue
+vim.g.terminal_color_5 = colors.magenta
+vim.g.terminal_color_13 = colors.magenta
+vim.g.terminal_color_6 = colors.cyan
+vim.g.terminal_color_14 = colors.cyan
+
 require("external").kitty.set_background(colors.bg)
 
 require("colors." .. O.colorscheme).overrides()
