e1413796d558689756dc1cef603854c61f584ab5
nuxsh <nuxshed@gmail.com>
2021-08-23 22:55:41 +0530

    nvim: fix gitsigns config

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -65,8 +65,6 @@ require("gitsigns").setup {
     follow_files = true,
   },
   current_line_blame = false,
-  current_line_blame_delay = 1000,
-  current_line_blame_position = "eol",
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
