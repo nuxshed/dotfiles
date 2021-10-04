0bf0fa4279b1778c825eb277a734268d6690562d
nuxsh <nuxshed@gmail.com>
2021-10-04 13:37:32 +0530

    remove polybar config

--- polybar/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar
-echo "---" | tee -a ~/.config/polybar/log
-polybar -c ~/.config/polybar/config.ini "$1" 2>&1 | tee -a  ~/.config/polybar/log & disown 
