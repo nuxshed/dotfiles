cafda60472f20116f9cd33bd4c3123b12100f974
nuxsh <nuxshed@gmail.com>
2021-10-30 17:41:51 +0530

    switch to yshui/picom

--- modules/desktop/picom.nix
+++ modules/desktop/picom.nix
@@ -1,5 +1,5 @@
 { config, pkgs, libs, ... }:
 {
-  home.packages = [ pkgs.picom-pijulius ];
+  home.packages = [ pkgs.picom-git ];
   home.file.".config/picom".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/picom";
 }
