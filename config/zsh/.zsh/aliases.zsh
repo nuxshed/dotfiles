835f9be1502a963a2fa81f40659f52cc7cdd8fa1
nuxsh <nuxshed@gmail.com>
2021-11-28 19:22:07 +0530

    zsh: replace auto_cd

--- config/zsh/.zsh/aliases.zsh
+++ config/zsh/.zsh/aliases.zsh
@@ -1,17 +1,17 @@
 alias vim="nvim"
-alias vi="/bin/vim"
 alias ls="exa"
 alias la="exa -a"
 alias li="exa --icons"
 alias lg="lazygit"
 alias btctl="bluetoothctl"
-alias pb="nc termbin.com 9999"
 alias grep="grep --color=auto"
 alias brightness="brightnessctl set"
 alias rotatescreen="xrandr --output eDP-1 --rotate"
 alias icat="kitty +kitten icat"
 
-# more .
+# replace AUTO_CD
+alias ~="cd ~"
+alias ..='cd ..'
 alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
