54fc9e08f0bc53d95b33b64f7c0f7d9164154585
nuxsh <nuxshed@gmail.com>
2021-12-30 22:28:27 +0530

    kitty: add nord theme

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -155,6 +155,44 @@
       # color13 = "#b6bdca";
       # color14 = "#be5046";
       # color15 = "#c8ccd4";
+
+      # nord --------------------------------------------------------
+      # background = "#2e3440";
+      # foreground = "#e5e9f0";
+      # selection_background = "#e5e9f0";
+      # selection_foreground = "#2e3440";
+      # url_color = "#d8dee9";
+      # cursor = "#e5e9f0";
+      # active_border_color = "#4c566a";
+      # inactive_border_color = "#3b4252";
+      # active_tab_background = "#2e3440";
+      # active_tab_foreground = "#e5e9f0";
+      # inactive_tab_background = "#3b4252";
+      # inactive_tab_foreground = "#d8dee9";
+      # tab_bar_background = "#3b4252";
+      #
+      # color0 = "#2e3440";
+      # color1 = "#bf616a";
+      # color2 = "#a3be8c";
+      # color3 = "#ebcb8b";
+      # color4 = "#81a1c1";
+      # color5 = "#b48ead";
+      # color6 = "#88c0d0";
+      # color7 = "#e5e9f0";
+      # color8 = "#616E88";
+      # color9 = "#bf616a";
+      # color10 = "#a3be8c";
+      # color11 = "#ebcb8b";
+      # color12 = "#81a1c1";
+      # color13 = "#b48ead";
+      # color14 = "#88c0d0";
+      # color15 = "#8fbcbb";
+      # color16 = "#d08770";
+      # color17 = "#5e81ac";
+      # color18 = "#3b4252";
+      # color19 = "#434c5e";
+      # color20 = "#d8dee9";
+      # color21 = "#eceff4";
     };
   };
 }
