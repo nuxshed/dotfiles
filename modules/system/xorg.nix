693b6ede0f68bbfaff614c2188a50f50ab566d90
nuxsh <nuxshed@gmail.com>
2021-12-31 22:28:40 +0530

    nixos: add berry

--- modules/system/xorg.nix
+++ modules/system/xorg.nix
@@ -5,13 +5,18 @@
     displayManager.gdm.enable = true;
     libinput.enable = true;
     layout = "gb";
-    windowManager.awesome = {
-      enable = true;
-      package = pkgs.awesome-git;
-    };
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
+    windowManager = {
+      awesome = {
+        enable = true;
+        package = pkgs.awesome-git;
+      };
+      i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+      };
+      berry = {
+        enable = true;
+      };
     };
   };
 }
