47c506a685a9c2bcf7200404214692412c27eaa2
nuxsh <nuxshed@gmail.com>
2022-01-07 20:24:21 +0530

    picom: convert config to nix

--- modules/desktop/picom.nix
+++ modules/desktop/picom.nix
@@ -1,5 +1,24 @@
 { config, pkgs, libs, ... }:
 {
-  home.packages = [ pkgs.picom-git ];
-  home.file.".config/picom".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/picom";
+  services.picom = {
+    enable = true;
+    package = pkgs.picom-git;
+    fade = true;
+    inactiveDim = "0.2";
+    shadow = true;
+    extraOptions = ''
+      shadow-radius = 40
+      shadow-opacity = 0.25
+      shadow-offset-x = -40
+      shadow-offset-y = -40
+    '';
+    shadowExclude = [
+      "name = 'Notification'"
+      "class_g = 'Conky'"
+      "class_g ?= 'Notify-osd'"
+      "class_g = 'Cairo-clock'"
+      "class_g = 'awesome'"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
+  };
 }
