b48d6c189b6b8da09014d7ecdbd208a4dab2b316
nuxsh <nuxshed@gmail.com>
2022-01-18 15:48:20 +0530

    vim: convert config to nix

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -1,5 +1,33 @@
 { config, pkgs, libs, ... }:
 {
-  home.file.".vimrc".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/vim/.vimrc";
+  home.file.".vimrc".text = ''
+    " bootstrap vim-plug
+    if empty(glob('~/.vim/autoload/plug.vim'))
+      silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+      \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    endif
+
+    call plug#begin()
+    Plug 'tpope/vim-commentary'
+    Plug 'tpope/vim-surround'
+    Plug 'sainnhe/gruvbox-material'
+    call plug#end()
+
+    set termguicolors
+    set background=dark
+    let g:gruvbox_material_background = 'hard'
+    colorscheme gruvbox-material
+
+    set number
+    set showtabline=2
+    set cursorline
+    set shiftwidth=2
+    set expandtab
+    set laststatus=0
+    set mouse=a
+
+    nnoremap <silent> <C-n> :tabnew<CR>
+    nnoremap <silent> <C-Left> :tabprevious<CR>
+    nnoremap <silent> <C-Right> :tabnext<CR>
+  '';
 }
