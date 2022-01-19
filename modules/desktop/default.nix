39f7dd2bd29359515e8f848b373f1601f1cf6d57
nuxsh <nuxshed@gmail.com>
2022-01-19 08:02:42 +0530

    nixos: use capitaine cursors

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -12,4 +12,14 @@
   ];
   home.file.".config/tint2".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/tint2";
+
+  xsession = {
+    enable = true;
+    pointerCursor = {
+      name = "capitaine-cursors";
+      package = pkgs.capitaine-cursors;
+      size = 32;
+    };
+    initExtra = "~/.fehbg";
+  };
 }
