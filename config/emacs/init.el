269879e952487b3e1eb32d4d04ed7fc1990c1650
nuxsh <nuxshed@gmail.com>
2022-01-19 18:39:29 +0530

    emacs: proper indenting

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -7,8 +7,8 @@
               vc-follow-symlinks)
 
 (setq undo-limit 80000000            ; moar undo
-    auto-save-default t              ; who knows what could happen?
-    truncate-string-ellipsis "…")    ; prettier ellipsis
+      auto-save-default t            ; who knows what could happen?
+      truncate-string-ellipsis "…")  ; prettier ellipsis
 
 ;; scrolling related settings
 (setq scroll-margin 1
@@ -20,11 +20,11 @@
 
 (setq inhibit-startup-echo-area-message t
       inhibit-startup-message t)
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode -1)          ; Disable the menu bar
-(scroll-bar-mode -1)        ; Disable the scrollbar
+(tool-bar-mode -1)    ; Disable the toolbar
+(tooltip-mode -1)     ; Disable tooltips
+(set-fringe-mode 10)  ; Give some breathing room
+(menu-bar-mode -1)    ; Disable the menu bar
+(scroll-bar-mode -1)  ; Disable the scrollbar
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
@@ -49,7 +49,7 @@
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-   (package-install 'use-package))
+  (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -62,15 +62,15 @@
   (evil-mode 1))
 
 (use-package evil-collection
-:after evil
-:config
-(evil-collection-init))
+  :after evil
+  :config
+  (evil-collection-init))
 
 (setq evil-cross-lines t
-    evil-move-beyond-eol t
-    evil-symbol-word-search t
-    evil-want-Y-yank-to-eol t
-    evil-cross-lines t)
+      evil-move-beyond-eol t
+      evil-symbol-word-search t
+      evil-want-Y-yank-to-eol t
+      evil-cross-lines t)
 
 (use-package evil-leader
   :config
@@ -120,7 +120,7 @@
 
 (use-package company
   :bind (:map company-active-map
-        ("<tab>" . company-select-next)))
+              ("<tab>" . company-select-next)))
 
 (use-package company-statistics
   :hook (company-mode . company-statistics-mode))
@@ -153,8 +153,8 @@
   :config
   (setq flycheck-emacs-lisp-load-path 'inherit)
   (setq flycheck-idle-change-delay 1.0)
-        (setq-local flycheck-emacs-lisp-initialize-packages t)
-        (setq-local flycheck-emacs-lisp-package-user-dir package-user-dir)
+  (setq-local flycheck-emacs-lisp-initialize-packages t)
+  (setq-local flycheck-emacs-lisp-package-user-dir package-user-dir)
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
 (use-package projectile
@@ -162,15 +162,18 @@
 
 (use-package magit)
 
+(use-package format-all
+  :init (format-all-mode))
+
 (with-eval-after-load 'dired
   (setq dired-dwim-target t
-    dired-listing-switches "-Alh"
-    dired-use-ls-dired t
-    dired-omit-files "\\`[.]?#\\|\\`[.][.]?\\|\\`[.].*\\'"
-    dired-always-read-filesystem t
-    dired-create-destination-dirs 'ask
-    dired-hide-details-hide-symlink-targets nil
-    dired-isearch-filenames 'dwim)
+        dired-listing-switches "-Alh"
+        dired-use-ls-dired t
+        dired-omit-files "\\`[.]?#\\|\\`[.][.]?\\|\\`[.].*\\'"
+        dired-always-read-filesystem t
+        dired-create-destination-dirs 'ask
+        dired-hide-details-hide-symlink-targets nil
+        dired-isearch-filenames 'dwim)
   (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))))
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
 (add-hook 'dired-mode-hook 'dired-omit-mode)
@@ -182,86 +185,86 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (with-eval-after-load 'ibuffer
   (setq ibuffer-expert t
-    ibuffer-show-empty-filter-groups nil)
+        ibuffer-show-empty-filter-groups nil)
   (defun my/human-readable-file-sizes-to-bytes (string)
     "Convert a human-readable file size into bytes."
     (interactive)
     (cond
-      ((string-suffix-p "G" string t)
-        (* 1000000000 (string-to-number (substring string 0 (- (length string) 1)))))
-      ((string-suffix-p "M" string t)
-        (* 1000000 (string-to-number (substring string 0 (- (length string) 1)))))
-      ((string-suffix-p "K" string t)
-        (* 1000 (string-to-number (substring string 0 (- (length string) 1)))))
-      (t
-        (string-to-number (substring string 0 (- (length string) 1))))))
+     ((string-suffix-p "G" string t)
+      (* 1000000000 (string-to-number (substring string 0 (- (length string) 1)))))
+     ((string-suffix-p "M" string t)
+      (* 1000000 (string-to-number (substring string 0 (- (length string) 1)))))
+     ((string-suffix-p "K" string t)
+      (* 1000 (string-to-number (substring string 0 (- (length string) 1)))))
+     (t
+      (string-to-number (substring string 0 (- (length string) 1))))))
 
   (defun my/bytes-to-human-readable-file-sizes (bytes)
     "Convert number of bytes to human-readable file size."
     (interactive)
     (cond
-      ((> bytes 1000000000) (format "%10.1fG" (/ bytes 1000000000.0)))
-      ((> bytes 100000000) (format "%10.0fM" (/ bytes 1000000.0)))
-      ((> bytes 1000000) (format "%10.1fM" (/ bytes 1000000.0)))
-      ((> bytes 100000) (format "%10.0fk" (/ bytes 1000.0)))
-      ((> bytes 1000) (format "%10.1fk" (/ bytes 1000.0)))
-      (t (format "%10d" bytes))))
+     ((> bytes 1000000000) (format "%10.1fG" (/ bytes 1000000000.0)))
+     ((> bytes 100000000) (format "%10.0fM" (/ bytes 1000000.0)))
+     ((> bytes 1000000) (format "%10.1fM" (/ bytes 1000000.0)))
+     ((> bytes 100000) (format "%10.0fk" (/ bytes 1000.0)))
+     ((> bytes 1000) (format "%10.1fk" (/ bytes 1000.0)))
+     (t (format "%10d" bytes))))
 
   ;; Use human readable Size column instead of original one
   (define-ibuffer-column size-h
     (:name "Size"
-      :inline t
-      :summarizer
-      (lambda (column-strings)
-        (let ((total 0))
-          (dolist (string column-strings)
-            (setq total
-              (+ (float (my/human-readable-file-sizes-to-bytes string))
-                total)))
-          (my/bytes-to-human-readable-file-sizes total)))
-      )
+           :inline t
+           :summarizer
+           (lambda (column-strings)
+             (let ((total 0))
+               (dolist (string column-strings)
+                 (setq total
+                       (+ (float (my/human-readable-file-sizes-to-bytes string))
+                          total)))
+               (my/bytes-to-human-readable-file-sizes total)))
+           )
     (my/bytes-to-human-readable-file-sizes (buffer-size)))
 
 (setq ibuffer-formats
-  '((mark modified read-only locked " "
-      (name 20 20 :left :elide)
-      " "
-      (size-h 11 -1 :right)
-      " "
-      (mode 16 16 :left :elide))
-     (mark " "
-       (name 16 -1)
-       " " filename))))
+      '((mark modified read-only locked " "
+              (name 20 20 :left :elide)
+              " "
+              (size-h 11 -1 :right)
+              " "
+              (mode 16 16 :left :elide))
+        (mark " "
+              (name 16 -1)
+              " " filename))))
 
 (setq ibuffer-saved-filter-groups
-  '(("main"
-      ("modified" (and
-                    (modified . t)
-                    (visiting-file . t)))
-      ("term" (or
-                (mode . vterm-mode)
-                (mode . eshell-mode)
-                (mode . term-mode)
-                (mode . shell-mode)))
-      ("config" (filename . "/dotfiles/"))
-      ("code" (filename . "/projects/"))
-      ("org" (mode . org-mode))
-      ("docs" (or
-                (mode . pdf-view-mode)
-                (mode . doc-view-mode)))
-      ("img" (mode . image-mode))
-      ("dired" (mode . dired-mode))
-      ("help" (or (name . "\*Help\*")
-                (name . "\*Apropos\*")
-                (name . "\*info\*")
-                (mode . help-mode)))
-      ("internal" (name . "^\*.*$"))
-      ("other" (name . "^.*$"))
-      )))
+      '(("main"
+         ("modified" (and
+                      (modified . t)
+                      (visiting-file . t)))
+         ("term" (or
+                  (mode . vterm-mode)
+                  (mode . eshell-mode)
+                  (mode . term-mode)
+                  (mode . shell-mode)))
+         ("config" (filename . "/dotfiles/"))
+         ("code" (filename . "/projects/"))
+         ("org" (mode . org-mode))
+         ("docs" (or
+                  (mode . pdf-view-mode)
+                  (mode . doc-view-mode)))
+         ("img" (mode . image-mode))
+         ("dired" (mode . dired-mode))
+         ("help" (or (name . "\*Help\*")
+                     (name . "\*Apropos\*")
+                     (name . "\*info\*")
+                     (mode . help-mode)))
+         ("internal" (name . "^\*.*$"))
+         ("other" (name . "^.*$"))
+         )))
 (add-hook 'ibuffer-mode-hook
-  (lambda ()
-    (ibuffer-auto-mode 1)
-    (ibuffer-switch-to-saved-filter-groups "main")))
+          (lambda ()
+            (ibuffer-auto-mode 1)
+            (ibuffer-switch-to-saved-filter-groups "main")))
 
 (use-package all-the-icons-ibuffer
   :ensure t
@@ -293,8 +296,8 @@
 (setq mu4e-trash-folder  "/Gmail/[Gmail]/Trash")
 
 (setq mu4e-get-mail-command "mbsync -a"
-  mu4e-compose-signature-auto-include nil
-  mu4e-compose-format-flowed t)
+      mu4e-compose-signature-auto-include nil
+      mu4e-compose-format-flowed t)
 
 (setq
  user-mail-address "nuxshed@gmail.com"
@@ -318,36 +321,36 @@
 (defun org/prettify-set ()
   (interactive)
   (setq prettify-symbols-alist
-      '(("#+begin_src" . "")
-        ("#+BEGIN_SRC" . "")
-        ("#+end_src" . "")
-        ("#+END_SRC" . "")
-        ("#+begin_example" . "")
-        ("#+BEGIN_EXAMPLE" . "")
-        ("#+end_example" . "")
-        ("#+END_EXAMPLE" . "")
-        ("#+results:" . "")
-        ("#+RESULTS:" . "")
-        ("#+begin_quote" . "❝")
-        ("#+BEGIN_QUOTE" . "❝")
-        ("#+end_quote" . "❞")
-        ("#+END_QUOTE" . "❞")
-        ("[ ]" . "☐")
-        ("[-]" . "◯")
-        ("[X]" . "☑"))))
+        '(("#+begin_src" . "")
+          ("#+BEGIN_SRC" . "")
+          ("#+end_src" . "")
+          ("#+END_SRC" . "")
+          ("#+begin_example" . "")
+          ("#+BEGIN_EXAMPLE" . "")
+          ("#+end_example" . "")
+          ("#+END_EXAMPLE" . "")
+          ("#+results:" . "")
+          ("#+RESULTS:" . "")
+          ("#+begin_quote" . "❝")
+          ("#+BEGIN_QUOTE" . "❝")
+          ("#+end_quote" . "❞")
+          ("#+END_QUOTE" . "❞")
+          ("[ ]" . "☐")
+          ("[-]" . "◯")
+          ("[X]" . "☑"))))
 (add-hook 'org-mode-hook 'org/prettify-set)
 
 (defun prog/prettify-set ()
   (interactive)
   (setq prettify-symbols-alist
-      '(("lambda" . "λ")
-        ("->" . "→")
-        ("<-" . "←")
-        ("<=" . "≤")
-        (">=" . "≥")
-        ("!=" . "≠")
-        ("~=" . "≃")
-        ("=~" . "≃"))))
+        '(("lambda" . "λ")
+          ("->" . "→")
+          ("<-" . "←")
+          ("<=" . "≤")
+          (">=" . "≥")
+          ("!=" . "≠")
+          ("~=" . "≃")
+          ("=~" . "≃"))))
 (add-hook 'prog-mode-hook 'prog/prettify-set)
 
 (global-prettify-symbols-mode)
