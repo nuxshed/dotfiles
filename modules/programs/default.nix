3e923cad8bc7b5a993fca780123eaf152a18da30
nuxsh <nuxshed@gmail.com>
2022-02-01 09:35:34 +0530

    nixos: configure bottom

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, libs, ... }:
 {
-  imports = [ ./irssi.nix ./kitty ];
+  imports = [ ./bottom.nix ./irssi.nix ./kitty ];
   home.packages = with pkgs; [ brave rofi ];
   home.file.".config/rofi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/rofi";
